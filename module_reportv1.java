68dbbc86f1b5b72e292a3e7c4db5e72aa57bf5f5
Dumb1245 <128900411+Dumb1245@users.noreply.github.com>
2023-07-26 17:50:51 +0530

    Update module_reportv1.java

--- module_reportv1.java
+++ module_reportv1.java
@@ -1,75 +1,53 @@
-import java.io.File;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 
-public class TextToPdfConverter {
-
+public class TextToPDF {
     public static void main(String[] args) {
-        String inputFilePath = "path/to/your/input_file.txt";
-        String outputFilePath = "path/to/your/output_file.pdf";
+        String inputFile = "input.txt";  // input text file
+        String outputFile = "output.pdf";  // output PDF file
 
+        Document document = new Document();
         try {
-            PDDocument document = new PDDocument();
-            PDPage page = new PDPage();
-            document.addPage(page);
+            PdfWriter.getInstance(document, new FileOutputStream(outputFile));
+            document.open();
+            
+            PdfPTable table = new PdfPTable(6);  // number of columns in the table
+            
+            // Add table headers
+            table.addCell("Header 1");
+            table.addCell("Header 2");
+            table.addCell("Header 3");
+            table.addCell("Header 4");
+            table.addCell("Header 5");
+            table.addCell("Header 6");
 
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-
-            // Define column headings
-            String[] headings = { "Column 1", "Column 2", "Column 3", "Column 4", "Column 5", "Column 6" };
-            float[] columnWidths = { 100, 100, 100, 100, 100, 100 };
-            float tableTopY = 700; // Adjust this value based on where you want to place the table on the page
-            float margin = 50;
-            float yPosition = tableTopY;
-
-            // Draw the table headings
-            drawTable(contentStream, yPosition, margin, headings, columnWidths);
-
-            // Parse the input text file
-            CSVParser parser = CSVParser.parse(new File(inputFilePath), java.nio.charset.StandardCharsets.UTF_8,
-                    CSVFormat.DEFAULT.withDelimiter('|'));
-
-            // Write the table data to the PDF
-            for (CSVRecord record : parser) {
-                yPosition -= 20; // Adjust the row height as needed
-                drawTable(contentStream, yPosition, margin, record.toArray(), columnWidths);
+            // Open the text file
+            BufferedReader br = new BufferedReader(new FileReader(inputFile));
+            String line;
+            
+            // Loop through each line in the file
+            while ((line = br.readLine()) != null) {
+                // Split the line by "|"
+                String[] parts = line.split("\\|");
+                
+                // Add a cell for each part
+                for (String part : parts) {
+                    table.addCell(part);
+                }
             }
-
-            contentStream.close();
-            document.save(outputFilePath);
-            document.close();
-
-            System.out.println("PDF created successfully.");
-        } catch (IOException e) {
+            br.close();
+            
+            // Add the table to the document
+            document.add(table);
+        } catch (DocumentException | IOException e) {
             e.printStackTrace();
+        } finally {
+            document.close();
         }
-    }
-
-    private static void drawTable(PDPageContentStream contentStream, float y, float margin, String[] content,
-            float[] columnWidths) throws IOException {
-        final int rows = 1;
-        final int cols = content.length;
-        final float rowHeight = 20f;
-        final float tableWidth = 600; // Adjust this value based on your page width
-
-        // Start the new row
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-        contentStream.beginText();
-        contentStream.moveTextPositionByAmount(margin, y);
-
-        for (int i = 0; i < cols; i++) {
-            // Draw the cell
-            contentStream.drawString(content[i]);
-            contentStream.moveTextPositionByAmount(columnWidths[i], 0);
-        }
-
-        contentStream.endText();
     }
 }
