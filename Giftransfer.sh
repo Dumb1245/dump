f56103a4451269b98d516fe7660b03777cd8b982
Dumb1245 <128900411+Dumb1245@users.noreply.github.com>
2024-06-20 18:24:29 +0530

    Update Giftransfer.sh

--- Giftransfer.sh
+++ Giftransfer.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# Define the shell command to be run on each BFA_DM11_* file
-SHELL_COMMAND="your_shell_command_here"
+# Directory where the files are located
+DIR="/path/to/your/files"
 
-# Loop through all files matching BDL_*ACKYINFO_UDP*.xml
-for bdl_file in BDL_*ACKYINFO_UDP*.xml; do
-    if [[ -e "$bdl_file" ]]; then
-        # Extract the common part of the filename to match BFA_DM11_* files
-        common_part=$(echo "$bdl_file" | sed -e 's/^BDL_\(.*\)_ACKYINFO_UDP.*\.xml$/\1/')
-        
-        # Find corresponding BFA_DM11_* files
-        for bfa_file in BFA_DM11_"$common_part"*.xml; do
-            if [[ -e "$bfa_file" ]]; then
-                # Append the content of the BDL file to the BFA file
-                cat "$bdl_file" >> "$bfa_file"
-                
-                # Run the specified shell command on the updated BFA file
-                $SHELL_COMMAND "$bfa_file"
-            fi
-        done
-    fi
+# Loop through each BDL_*ACKYINFO_UDP*.xml file
+for bdl_file in "${DIR}"/BDL_*ACKYINFO_UDP*.xml; do
+  # Extract the unique identifier from the BDL file name
+  unique_id=$(basename "$bdl_file" | sed 's/^BDL_//;s/_ACKYINFO_UDP.*\.xml$//')
+  
+  # Find the corresponding BFA_DM11_* file
+  bfa_file=$(find "$DIR" -type f -name "BFA_DM11_${unique_id}*.xml")
+  
+  if [[ -f "$bfa_file" ]]; then
+    # Append the content of the BDL file to the BFA file
+    cat "$bdl_file" >> "$bfa_file"
+    
+    # Run the shell command using the BFA file
+    # Replace `your_command_here` with the actual command you want to run
+    your_command_here "$bfa_file"
+  else
+    echo "No corresponding BFA_DM11_* file found for $bdl_file"
+  fi
 done
